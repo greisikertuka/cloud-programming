25f12e52258269891d695b3bbf94fd592304a819
Greisi <greisikertuka@gmail.com>
2024-03-26 17:38:54 +0100

    Add files for the first time

---
+++ src/main/java/com/lab1/programimicloudlab1/CloudProgrammingLab1Application.java
@@ -0,0 +1,13 @@
+package com.lab1.programimicloudlab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudProgrammingLab1Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        SpringApplication.run(CloudProgrammingLab1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lab1/programimicloudlab1/ProgramimiCloudLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab1.programimicloudlab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramimiCloudLab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
